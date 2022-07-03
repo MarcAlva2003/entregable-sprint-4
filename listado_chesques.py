d4c3a43a015679866c88b0f4a043c130bc85a403
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-07-03 15:57:22 -0300

    Merge: py file

--- listado_chesques.py
+++ listado_chesques.py
@@ -13,42 +13,23 @@ import csv
     
 #     return True
 
-def StartApp():
-    valores = ingresarValores()
-    data = abrirArchivoCSV(valores[0])
-    if not(checkDni (valores[1], data[1], data[0].index("DNI"), data[0].index("NroCheque"))) :
-        print ("El numero de cheque se repite con el mismo DNI")
-    # if(checkDni(valores[1], data[1]) -> return boolean)
-    #   true -> sigue
-    #   false -> tire error
-    # dataFiltrada = obtenerDataFiltrada(valores[], content[]) -> Obtener cheques que cumplan con los filtros
-                #  content = filtrarPorDni(valores[1], content)
-                #  content = filtrarPorTipoCheque(valores[3], content)
-                #  if(data[4] -> es un input salteado -> false -> usar filtro
-                #   data[4] -> es un input salteado -> true -> NO usar filtro
-                #   data[5] -> es un input salteado -> false -> usar filtro
-                #   data[5] -> es un input salteado -> true -> NO usar filtro)
-
-    # retornarValores(dataFiltrada, valores[2])
-
-#StartApp()
 
 #------------------------------------------------------------------------------------------------
 
-def abrirArchivoCSV(nombreArchivo):
-    """Abre el archivo y retorna el encabezado y crea una lista con los datos """
-    archivo = open(nombreArchivo, 'r')
-    reader = csv.reader(archivo)
-    header = []
-    listado = []
-    condision = True
-    for row in reader:
-        if condision == 0:
-            header = row
-            condision = False
-        else: 
-            listado.append(row)
-    return [header] + listado
+# def abrirArchivoCSV(nombreArchivo):
+#     """Abre el archivo y retorna el encabezado y crea una lista con los datos """
+#     archivo = open(nombreArchivo, 'r')
+#     reader = csv.reader(archivo)
+#     header = []
+#     listado = []
+#     condision = True
+#     for row in reader:
+#         if condision == 0:
+#             header = row
+#             condision = False
+#         else: 
+#             listado.append(row)
+#     return [header] + listado
 
 
 def abrirArchivoCSV2(nombreArchivo):
@@ -56,28 +37,29 @@ def abrirArchivoCSV2(nombreArchivo):
     reader = csv.reader(archivo)
     header = []
     listado = []
-    contador = 0
+    contador = True
     for row in reader:
-        if contador == 0:
+        if contador:
             header = row
-            contador += 1
+            contador = False
         else: 
             listado.append(row)
+    #Cerrar
     return [header, listado]
 
 
-def iniciarData():
-    """Confirma el nombre del Archivo y llama a la funcion "Abrir archivo" """
-    validacion_archivo = input('Si el nombre del archivo a trabajar es data.csv ingrese SI de otra manera NO:  ')
-    condicion = True
-    while condicion:
-        if validacion_archivo == 'SI' or validacion_archivo == 'si':
-            condicion = False
-            return abrirArchivoCSV('data.csv')
-        elif validacion_archivo == 'NO' or validacion_archivo == 'no':
-            nombreArchivo = input("Ingresa el nombre del archivo : ")
-            condicion = False 
-            return abrirArchivoCSV(nombreArchivo)
+# def iniciarData():
+#     """Confirma el nombre del Archivo y llama a la funcion "Abrir archivo" """
+#     validacion_archivo = input('Si el nombre del archivo a trabajar es data.csv ingrese SI de otra manera NO:  ')
+#     condicion = True
+#     while condicion:
+#         if validacion_archivo == 'SI' or validacion_archivo == 'si':
+#             condicion = False
+#             return abrirArchivoCSV('data.csv')
+#         elif validacion_archivo == 'NO' or validacion_archivo == 'no':
+#             nombreArchivo = input("Ingresa el nombre del archivo : ")
+#             condicion = False 
+#             return abrirArchivoCSV(nombreArchivo)
 
 
 def iniciarData2():
@@ -99,17 +81,18 @@ def variableDNI():
     variable_dni = input("Numero DNI : ")
     while  not(str.isdigit(variable_dni)):
         print("Dni no valido")
-        variable_dni = input("Numero DNI : ")        
+        variable_dni = input("Numero DNI : ")
     return variable_dni
 
 
-def checkDni (dni, content, indexDNI, indexNroCheque) :
+def checkDni (dni, content, indexDNI, indexNroCheque):
     # print (dni, content, indexDNI, indexNroCheque)
     nrosCheques = []
     for row in content: 
         if row[indexDNI] == dni:
             if row[indexNroCheque] in nrosCheques:
                 print ("El numero de cheque se repite con el mismo DNI")
+                return False
             else: 
                 nrosCheques.append(row[indexNroCheque]) 
     
@@ -158,7 +141,7 @@ def filtrarPorTipo(tipo, indexTipo, data):
     pass
 
 def filtro(dni, tipoCheque, estadoCheque, rangoFecha, data):
-    """Filtra la data que se obtinee de data.csv utilizando los parametros ingresados por el cliente"""
+    """Retorna la data que se obtinee de data.csv utilizando los parametros ingresados por el cliente"""
     lineasARetornar = []
     indexDni = data[0].index("DNI")
     indexTipo  = data[0].index('Tipo')
@@ -168,12 +151,11 @@ def filtro(dni, tipoCheque, estadoCheque, rangoFecha, data):
     # if not(fecha == ''):
     #     lista = filtratFecha(lista, tipo indexTIPO)
     # retutn listra
-    
 
     for row in data[1]:
         if (row[indexDni] == dni) and (row[indexTipo] == tipoCheque):
             lineasARetornar.append(row)
-    
+
     return lineasARetornar
 
 # # # # # # # # # # # 
@@ -213,10 +195,10 @@ def Estado_de_cheques(dni,lista_de_datos):
 def ontenerEstadoCheque(dni,tipo_cheque):
     """Obtiene a partir de un dni y el tipo de cheque su estado"""
 
-    # estadoCheque = input("PENDIENTE, APROBADO, RECHAZADO : ")
-    # while estadoCheque != "PENDIENTE" and estadoCheque != "APROBADO" and estadoCheque != "RECHAZADO" and estadoCheque != "" :
-    #     estadoCheque = input('Ingrese un valor correcto(PENDIENTE, APROBADO O RECHAZADO)')
-    # return estadoCheque
+    estadoCheque = input("PENDIENTE, APROBADO, RECHAZADO : ")
+    while estadoCheque != "PENDIENTE" and estadoCheque != "APROBADO" and estadoCheque != "RECHAZADO" and estadoCheque != "" :
+        estadoCheque = input('Ingrese un valor correcto(PENDIENTE, APROBADO O RECHAZADO)')
+    return estadoCheque
     
 
 
@@ -244,9 +226,10 @@ def StartApp2():
     # lista_lista_datos = iniciarData()
     lista_datos2 = iniciarData2() # = [ header, content[] ]
     dni_ingresado = variableDNI() # = dni
-    Estado_de_cheques(dni_ingresado,lista_datos2)
+    # Estado_de_cheques(dni_ingresado,lista_datos2)
 
     salida = tipo_Salida() # = salida
+
     tipoCheque = obtenerTipoCheque() # = tipoCheque
     estadoCheque = ontenerEstadoCheque(dni_ingresado,tipoCheque) # = estadoCheque o ''
     rangoFecha = obtenerRangoFecha() # = rangoFecha o ''
@@ -254,7 +237,7 @@ def StartApp2():
     dataFiltrada = filtro(dni_ingresado, tipoCheque, estadoCheque, rangoFecha, lista_datos2)
     for row in dataFiltrada:
         print(row)
-    
+
     # NO BORRAR XD
     # for row in dataFiltrada:
     #     indice = 0
@@ -264,13 +247,15 @@ def StartApp2():
     #         indice += 1
     #     print(cadena)
 
+
     header =  ['NroCheque','CodigoBanco','CodigoScurusal','NumeroCuentaOrigen','NumeroCuentaDestino','Valor','FechaOrigen','FechaPago','DNI','Tipo','Estado']
+    
     dataFiltradaMock = [
         ['0002','55','44','2432432423','343434343','5559,76','1620183371','1620183371','23665789','EMITIDO','PENDIENTE'],
         ['0002','55','44','2432432423','343434343','5559,76','1620183371','1620183371','23665789','EMITIDO','PENDIENTE'],
         ['0002','55','44','2432432423','343434343','5559,76','1620183371','1620183371','23665789','EMITIDO','PENDIENTE'],
         ['0002','55','44','2432432423','343434343','5559,76','1620183371','1620183371','23665789','EMITIDO','PENDIENTE'],
-        ]
+    ]
 
     # mostrarValres(dataFiltradaMock, salida);
 
